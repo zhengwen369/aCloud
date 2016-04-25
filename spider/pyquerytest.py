735ee1c9b5880c9ffeb4f6b077584447d9cb885c
haocy <haocy@radiobuy.cn>
2016-04-25 17:23:05 +0800

    重写PyQuery调用事例

--- spider/pyquerytest.py
+++ spider/pyquerytest.py
@@ -1,13 +1,6 @@
 #  -*- coding: utf-8 -*-
-from pyquery import PyQuery
+from pyquery import PyQuery as PyQ
 
-v_source = PyQuery(url='http://yunvs.com/list/mai_1.html')
-for data in v_source('tr'):
-    v_code = PyQuery(data).find('td').eq(0).text()
-    v_name = PyQuery(data).find('td').eq(1).text()
-    v_ind = PyQuery(data).find('td').eq(5)
-    for i in range(len(PyQuery(v_ind).find('a'))):
-        v_indname = PyQuery(v_ind).find('a').eq(i).text()
-        print v_code
-        print v_name
-        print v_indname
+d = PyQ("http://www.ximalaya.com/dq/all/")
+for sort_list in [x.text for x in d('#discoverAlbum .sort_list li a') if d(x).parent().attr('cid') != '0']:
+    print sort_list
